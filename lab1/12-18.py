9d189d12f842737da9d342d052562715061d0dbe
KinomiTS <razorrar80@gmail.com>
2024-02-05 01:33:05 +0600

    lab1

---
+++ lab1/12-18.py
@@ -0,0 +1,35 @@
+x=5
+print(type(x))
+
+#Answer - int
+
+x = "Hello World"
+print(type(x))
+
+#Answer - string
+
+x = 20.5
+print(type(x))
+
+#Answer - float
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+#Answer - list
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+#Answer - tuple
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+#Answer - dictionary
+
+x = True
+print(type(x))
+
+#Answer - boolean
+
