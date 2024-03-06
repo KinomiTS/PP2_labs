b23cc2ad62a1808dd063b6456bbf77f2c3e06edd
KinomiTS <razorrar80@gmail.com>
2024-03-06 21:14:32 +0600

    done

---
+++ lab4/prlgrm.py
@@ -0,0 +1,11 @@
+import math
+
+def prlgrmArea(lngt, hgt):
+    S = lngt * hgt
+    return S
+
+a=int(input())
+b=int(input())
+
+S = prlgrmArea(a, b)
+print("The area of parallelogram is: ", S)
---
+++ lab4/rad.py
@@ -0,0 +1,6 @@
+import math 
+
+dgr=int(input("Input degree:"))
+rad= math.radians(dgr)
+
+print("We get this radian: ", rad)
